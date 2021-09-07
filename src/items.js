cdb848701e67e32813388db5f1fbe234cafa2eb2
Valeron <Lerok.vasil@outlook.com>
2021-09-07 23:02:07 +0500

    add homework

---
+++ src/items.js
@@ -0,0 +1,57 @@
+import image1 from "./assets/img/1.jpg";
+import image2 from "./assets/img/2.jpg";
+import image3 from "./assets/img/3.jpg";
+import image4 from "./assets/img/4.gif";
+import audio1 from "./assets/audio/1.mp3";
+import audio2 from "./assets/audio/2.mp3";
+import audio3 from "./assets/audio/3.mp3";
+import video1 from "./assets/video/1.mp4";
+import video2 from "./assets/video/2.mp4";
+
+export const items = [
+  {
+    url: image1,
+    type: "image",
+    label: "Dream house",
+  },
+  {
+    url: image2,
+    type: "image",
+    label: "Water",
+  },
+  {
+    url: image3,
+    type: "image",
+    label: "Many houses",
+  },
+  {
+    url: image4,
+    type: "image",
+    label: "Wow... mountain",
+  },
+  {
+    url: audio1,
+    type: "audio",
+    label: "Birds and nature",
+  },
+  {
+    url: audio2,
+    type: "audio",
+    label: "Some dudki",
+  },
+  {
+    url: audio3,
+    type: "audio",
+    label: "Silent",
+  },
+  {
+    url: video1,
+    type: "video",
+    label: "Dance time",
+  },
+  {
+    url: video2,
+    type: "video",
+    label: "Waves wshhh",
+  },
+];
